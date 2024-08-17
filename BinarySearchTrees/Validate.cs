f1209ca70393d1fd420653aeb058e31cf5d435af
noctvrno <feed.faceit@gmail.com>
2024-08-17 21:34:49 +0300

    Validate BinarySearchTree

---
+++ BinarySearchTrees/Validate.cs
@@ -0,0 +1,40 @@
+﻿namespace Leet.BinarySearchTrees;
+
+internal static class Validate
+{
+    public static bool IsValidBST(TreeNode node)
+    {
+        List<TreeNode> flattenedTree = [];
+        TraverseBst(node, flattenedTree);
+        return flattenedTree
+            .OrderBy(node => node.val)
+            .Select(node => node.val)
+            .Distinct()
+            .SequenceEqual(flattenedTree.Select(node => node.val));
+    }
+
+    private static void TraverseBst(TreeNode node, ICollection<TreeNode> result)
+    {
+        if (node.left != null)
+            TraverseBst(node.left, result);
+
+        result.Add(node);
+
+        if (node.right != null)
+            TraverseBst(node.right, result);
+    }
+}
+
+internal class TreeNode
+{
+    public readonly int val;
+    public readonly TreeNode? left;
+    public readonly TreeNode? right;
+
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Leet.BinarySearchTrees;
+
+// //var root = new TreeNode(
+//    5,
+//    new TreeNode(1),
+//    new TreeNode(
+//        4,
+//        new TreeNode(3),
+//        new TreeNode(6)));
+
+//var root = new TreeNode(
+//    5,
+//    new TreeNode(4),
+//    new TreeNode(
+//        6,
+//        new TreeNode(3),
+//        new TreeNode(7)));
+
+//var root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+
+var root = new TreeNode(2, new TreeNode(2), new TreeNode(2));
+
+Console.WriteLine(Validate.IsValidBST(root));
