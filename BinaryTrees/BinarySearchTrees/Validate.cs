51116644d8a213ac47d6334f2a7b62ce4660d154
noctvrno <feed.faceit@gmail.com>
2024-08-17 22:19:17 +0300

    Invert BinaryTree

---
+++ BinaryTrees/BinarySearchTrees/Validate.cs
@@ -0,0 +1,27 @@
+﻿namespace Leet.BinaryTrees.BinarySearchTrees;
+
+internal static class Validate
+{
+    public static bool IsValidBST(TreeNode node)
+    {
+        List<TreeNode> flattenedTree = [];
+        TraverseBst(node, flattenedTree);
+        return flattenedTree
+            .OrderBy(node => node.val)
+            .Select(node => node.val)
+            .Distinct()
+            .SequenceEqual(flattenedTree.Select(node => node.val));
+    }
+
+    private static void TraverseBst(TreeNode node, ICollection<TreeNode> result)
+    {
+        if (node.left != null)
+            TraverseBst(node.left, result);
+
+        result.Add(node);
+
+        if (node.right != null)
+            TraverseBst(node.right, result);
+    }
+}
+
---
+++ BinaryTrees/Invert.cs
@@ -0,0 +1,33 @@
+﻿namespace Leet.BinaryTrees;
+
+public static class Invert
+{
+    public static TreeNode? InvertTree(TreeNode? root)
+    {
+        if (root == null)
+            return null;
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+
+            if (node.left != null)
+                queue.Enqueue(node.left);
+
+            if (node.right != null)
+                queue.Enqueue(node.right);
+
+            SwapNodes(ref node.left, ref node.right);
+        }
+
+        return root;
+    }
+
+    private static void SwapNodes(ref TreeNode? left, ref TreeNode? right)
+    {
+        (left, right) = (right, left);
+    }
+}
